8d57678fe3c4344317b28592bb0828eec6bccb62
azhou16 <angelz168@gmail.com>
2015-07-29 09:16:48 -0700

    Final Changes

--- Chart.js
+++ Chart.js
@@ -4,32 +4,30 @@
     function drawChart() {
 
       var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Day');
-      data.addColumn('number', 'Own Usage');
-      data.addColumn('number', 'Local Average Usage');
+      data.addColumn('number', 'Month');
+      data.addColumn('number', 'Your Home Usage');
+      data.addColumn('number', 'Similar Homes\' Usage');
       data.addColumn('number', 'Recommmended  Usage');
 
       data.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
+        [1,  480, 550, 500],
+        [2,  490, 620, 485],
+        [3,  430, 540, 410],
+        [4,  400, 500, 390],
+        [5,  455, 550, 385],
+        [6,   440, 475, 350],
+        [7,   415, 530, 370],
+        [8,  435, 605, 410],
+        [9,  500, 625, 399],
+        [10, 385, 520, 405],
+        [11,  395, 500, 400],
+        [12,  475,  620, 386]
       ]);
 
       var options = {
         chart: {
-          title: 'Box Office Earnings in First Two Weeks of Opening',
-          subtitle: 'in millions of dollars (USD)'
+          title: 'Last 12 Months Household Comparison',
+          subtitle: 'Electricity \(kWh)'
         },
         width: 900,
         height: 500
