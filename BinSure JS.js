b4dbd2da45c4120a13d6c1eba9214f54908728de
azhou16 <angelz168@gmail.com>
2015-07-29 09:40:15 -0700

    Final Updates 2

--- BinSure JS.js
+++ BinSure JS.js
@@ -53,7 +53,7 @@ var plastic=["water bottles","plastic","plastic forks", "plastic bags", "plastic
  var good_tip=plastic.indexOf(search);
  if (good_tip!=-1)
 	{
-		document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px"><img src="Tip Icon.png" height=100px></div><h3>Tip:</h3> <p> In general plastics that are rigid and cannot be easily ripped apart by hand are recyclable <br> while soft, easily-broken plastics must go in the trash. <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 620px"> See All Tips </a> </p> '
+		document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px"><img src="Tip Icon.png" height=100px></div><h3>Tip:</h3> <p> In general plastics that are rigid and cannot be easily ripped apart by hand are recyclable <br> while soft, easily-broken plastics must go in the trash. <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 850px"> See All Tips </a> </p> '
 	   
 	}
 
@@ -63,17 +63,17 @@ else
 	  var good_tip2=food_soiled.indexOf(search);
 	  if (good_tip2!=-1)
 	  {
-		  document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px"><img src="Tip Icon.png" height=100px></div><h3>Tip:</h3> <p> All paper products that have been food-soiled should be put in the compost.</p> <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 620px"> See All Tips </a>'
+		  document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px"><img src="Tip Icon.png" height=100px></div><h3>Tip:</h3> <p> All paper products that have been food-soiled should be put in the compost.</p> <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 850px"> See All Tips </a>'
 	       
 	  }
 	  else 
 		  
 		{ if (search=="styrofoam")
-			{ document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px; margin-top: -200px"><img src="Tip Icon.png" height=100px></div><h3 style="margin-top: -100px">Tip:</h3> <p> Styrofoam peanuts can be dropped off at many mail houses for reuse.</p> <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 620px"> See All Tips </a>'
+			{ document.getElementById("tip").innerHTML = '<div id="bulb" style= "margin-bottom: -75px; margin-left: -70px; margin-top: -200px"><img src="Tip Icon.png" height=100px></div><h3 style="margin-top: -65px">Tip:</h3> <p> Styrofoam peanuts can be dropped off at many mail houses for reuse.</p> <br> <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 850px"> See All Tips </a>'
 				
 			}
 		else {
-				document.getElementById("tip").innerHTML = ' <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 620px"> <p> Tips </p><img src="Tip Icon.png" style= "margin-left: -70px; height: 100px; margin-top: -100px"></a>'
+				document.getElementById("tip").innerHTML = ' <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 850px"> <p> Tips </p><img src="Tip Icon.png" style= "margin-left: -70px; height: 100px; margin-top: -100px"></a>'
 				//get spacing right 
 			}   
 		}
@@ -85,21 +85,21 @@ function recycling_function()
 document.getElementById("ans").innerHTML =
 			'<h1>Recycling</h1><div id="bin"> <img src="Recycle Bin.png" height=500px></div>';
 			document.getElementById("message").innerHTML =
-		"<p>You need to put " + search + " in the recycling. </p> ";
+		"<br><p>You need to put " + search + " in the recycling. </p> ";
 		
 			
 	};
 function compost_function()
 {document.getElementById("ans").innerHTML = '<h1>Compost</h1><div id="bin"> <img src="Compost Bin.png" height=500px> </div>';
 	document.getElementById("message").innerHTML =
-		"<p>You need to put " + search + " in the compost. </p> ";
+		"<br><p>You need to put " + search + " in the compost. </p> ";
 }
 function trash_function()
 {
 	document.getElementById("ans").innerHTML = 
 	'<h1>Trash</h1><div id="bin"> <img src="Landfill Bin.png" height=500px> </div>';
 	document.getElementById("message").innerHTML =
-		"<p>You need to put " + search + " in the trash. </p> ";
+		"<br><p>You need to put " + search + " in the trash. </p> ";
 		
 }   
   function special_function() 
@@ -108,17 +108,17 @@ function trash_function()
   
   if (search=="medicine")
 			{
-			document.getElementById("message").innerHTML ="<p> You need to drop medicine off at a police station or at certain pharmacies. </p>  <em> Medicine flushed down the drain or disposed in the garbage can find its way into the water supply!</em>";
+			document.getElementById("message").innerHTML ="<br><p> You need to drop medicine off at a police station or at certain pharmacies. </p>  <em> Medicine flushed down the drain or disposed in the garbage can find its way into the water supply!</em>";
 			}
   else  if  (search=="clothes")
 			 
 			 {
-				 document.getElementById("message").innerHTML ='<p> You need to fold up clothes in a cardboard box and set up a special pick up with the </p>  <a href="http://www.recyclemyjunk.com/" target="_blank"> RecycleMyJunk Program </a> ';
+				 document.getElementById("message").innerHTML ='<br><p> You need to fold up clothes in a cardboard box and set up a special pick up with the </p>  <a href="http://www.recyclemyjunk.com/" target="_blank"> RecycleMyJunk Program </a> ';
 			 }
 				 		 
 			 else
 			 {
-				document.getElementById("message").innerHTML = ' <div><p> You need to take ' + search +  ' directly to the  <a href="http://www.recologysf.com/index.php/for-homes/transfer-station-residential" target="_blank"> dump </a> or schedule a pick up with the  <a href="http://www.recyclemyjunk.com/" target="_blank"> RecycleMyJunk Program</a>. </p> </div> ';
+				document.getElementById("message").innerHTML = ' <div><br><p> You need to take ' + search +  ' directly to the  <a href="http://www.recologysf.com/index.php/for-homes/transfer-station-residential" target="_blank"> dump </a> or schedule a pick up with the  <a href="http://www.recyclemyjunk.com/" target="_blank"> RecycleMyJunk Program</a>. </p> </div> ';
 			 }
 		 
   };
@@ -132,7 +132,7 @@ function notFound() {
 	document.getElementById("ans").innerHTML = 
 	'<h1 id="notFound">Item not found.</h1><div id="404bin" style="margin-top: -200px"> <img src="Not Found Bin.png" height=500px> </div>'; //placeholder img
 	document.getElementById("message").innerHTML =
-		'<b>Tip:</b><em> Is your spelling correct?</em> <br> <em> If so try searching for similar items or choosing similar items from the suggestion bar. </em><br><br>'; //create quick tips here for plastic and food items and common materials
+		'<b>Tip:</b><em> Is your spelling correct?</em> <br> <em> If so try searching for similar items or choosing similar items from the suggestion bar. </em><br>'; //create quick tips here for plastic and food items and common materials
                   
 		document.getElementById("tip").innerHTML = '   <a href="#pagetwo" class="ui-btn"style="height: 35px; width: 100px; margin-left: 335px; margin-top: 300px"> <p> Tips </p><img src="Tip Icon.png" style= "margin-left: -70px; height: 100px; margin-top: 200px"></a>'
 		}
